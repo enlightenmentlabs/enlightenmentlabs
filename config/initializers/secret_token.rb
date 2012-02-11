f8a6d37ae341ca3d40635f3964b36449c9671c1c
Adam Strickland <adam.strickland@gmail.com>
2012-02-11 10:21:50 -0600

    initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enlightenmentlabs::Application.config.secret_token = 'c6476ebf7d1c9b72846f8f1dc9f3bd942505e95cf52a60697ceb932587c07cb93855f3fd2c5f7a514bf51acab46c8d8eaf64fd5fe33f90975baee56a5719efad'
